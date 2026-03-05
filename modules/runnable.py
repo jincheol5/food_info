bb28751a990774fc2738039177c165c497252093
jincheol <oscar4293@gmail.com>
2026-03-05 15:20:32 +0900

    update

--- modules/runnable.py
+++ modules/runnable.py
@@ -1,6 +1,44 @@
+import os
+import torch
 from langchain_core.runnables import Runnable
+from .utils import ModelUtils
 
 class DeepSeek_OCR_Runnable(Runnable):
-    def __init__(self,config):
+    def __init__(self,config:dict):
         """
         """
+        self.model=ModelUtils.load_local_llm(
+            model_name="DeepSeek-OCR",
+            model_type="base",
+            config=config
+        )
+        self.tokenizer=ModelUtils.load_local_tokenizer(
+            model_name="DeepSeek-OCR",
+            config=config
+        )
+        self.output_path=config['output']
+        os.makedirs(self.output_path,exist_ok=True) 
+
+    def set_output_path(self,output_path):
+        self.output_path=output_path
+
+    def invoke(self,image_path):
+        prompt="<image>\nFree OCR."
+        with torch.no_grad():
+            self.model.infer(
+                self.tokenizer,
+                prompt=prompt,
+                image_file=image_path,
+                output_path=self.output_path,
+                base_size=640,   
+                image_size=640,
+                crop_mode=False,
+                save_results=True
+            )
+
+        print(f"<<OCR result>>",end="\n\n")
+        result_file=self.output_path / "result.mmd"
+        with open(result_file,"r",encoding="utf-8") as f:
+            raw_text=f.read()
+        return raw_text
+
--- test/test2.py
+++ test/test2.py
@@ -1,70 +1,18 @@
 import torch
-from modules import ModelUtils
+from modules import DeepSeek_OCR_Runnable
 from pathlib import Path
-from langchain_core.runnables import RunnableLambda
-
-def run_deepseek_ocr(inputs:dict):
-    """
-    inputs
-        model
-        tokenizer
-        image_path
-    """
-    model=inputs['model']
-    tokenizer=inputs['tokenizer']
-    image_path=inputs['image_path']
-    prompt="<image>\nFree OCR."
-    output_path=Path(__file__).resolve().parent.parent / "ocr_results"
-
-    with torch.no_grad():
-        model.infer(
-            tokenizer,
-            prompt=prompt,
-            image_file=image_path,
-            output_path=output_path,
-            base_size=640,   
-            image_size=640,
-            crop_mode=False,
-            save_results=True
-        )
-
-    print(f"<<OCR result>>",end="\n\n")
-    result_file=output_path / "result.mmd"
-    with open(result_file,"r",encoding="utf-8") as f:
-        raw_text=f.read()
-    return raw_text
 
 def main(config:dict):
-    model=ModelUtils.load_local_llm(
-        model_name="DeepSeek-OCR",
-        model_type="base",
-        config=config
-    )
-    model=model.eval().cuda()
-
-    tokenizer=ModelUtils.load_local_tokenizer(
-        model_name="DeepSeek-OCR",
-        config=config
-    )
-
+    vlm_runnable=DeepSeek_OCR_Runnable(config=config)
     image_path=Path(__file__).resolve().parent.parent / "food" / "nutrition_1.png"
-    inputs={
-        "model":model,
-        "tokenizer":tokenizer,
-        "image_path":image_path
-    }
-
-    ocr_runnable=RunnableLambda(run_deepseek_ocr)
-    raw_text=ocr_runnable.invoke(inputs)
-
-    ### raw_text -> json 코드 작성
-
-
+    raw_text=vlm_runnable.invoke(image_path=image_path)
+    print(raw_text)
 
 if __name__=="__main__":
     config={
         "torch_dtype": torch.bfloat16,
         "trust_remote_code": True,
-        "use_safetensors": True
+        "use_safetensors": True,
+        "output_path": Path(__file__).resolve().parent.parent / "ocr_results"
     }
     main(config=config)
